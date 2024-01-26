00c0f26badac1efe60ade36932fcdc800b79d776
Rhuan M. Mulero <148873205+rhuanmulero@users.noreply.github.com>
2024-01-26 11:30:15 -0300

    Add files via upload

---
+++ parimpar.js
@@ -0,0 +1,15 @@
+const numero = 7;
+
+for (let i = 1; i < 11; i++) {
+    console.log(i * numero);
+    
+
+   if (numero * i % 2 === 0) {
+console.log(`é par`)
+
+   } else if (numero % 2 != 0) {
+
+    console.log(`é impar`)
+
+   }
+}
